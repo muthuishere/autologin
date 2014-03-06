9867ed31985c062c054aa0bbcd24ed04232a5f02
Muthukumaran <muthuishere@gmail.com>
2014-03-06 17:58:25 +0530

    options

--- scripts/options.js
+++ scripts/options.js
@@ -340,3 +340,16 @@ row.innerHTML =	 "<td style='text-align:left'>"+ autoLoginInfo.domain+"</td>"+
 
 
 window.addEventListener('load', autoLoginOptions.init);
+
+
+
+/*
+
+
+var myString = '<div style="position:fixed;border:2px solid;border-radius:25px;background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#7abcff), color-stop(44%,#60abf8), color-stop(100%,#4096ee)); ;color:-webkit-gradient(linear, left top, left bottom, color-stop(0%,#4c4c4c), color-stop(12%,#595959), color-stop(25%,#666666), color-stop(39%,#474747), color-stop(50%,#2c2c2c), color-stop(51%,#000000), color-stop(60%,#111111), color-stop(76%,#2b2b2b), color-stop(91%,#1c1c1c), color-stop(100%,#131313));right:202px;top:0px;height:200px;width:200px">   <table id="" style="width:100%" > <tbody> <tr> <th>Enter Master Password to activate AutoLogin:</th></tr><tr> <th> <input type="password" id="txtaskpassword" /> </th> </tr><tr>   <th> <input type="button" id="btnaskpassword" value="Activate" /> </th>   </tr> </tbody> </table> </div>'
+
+var container=document.createElement("div");
+container.innerHTML=myString
+document.body.appendChild(container)
+
+*/
